3276ad6c7931928ef958bf2c8c741f9b1eac83b1
Robbie Behm <behmaster@gmail.com>
2022-06-09 18:38:56 -0500

    sending index and show data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -3,20 +3,22 @@ const breads = express.Router();
 const Bread = require("../models/bread.js");
 
 // INDEX
-breads.get('/', (req, res) => {
-    res.render('Index',
-      {
-        breads: Bread
-      }
-    )
-  // res.send(Bread)
-})
-
-
+breads.get("/", (req, res) => {
+  res.render("Index", {
+    breads: Bread,
+    title: "Index Page",
+  });
+});
 
 // SHOW
 breads.get("/:arrayIndex", (req, res) => {
-  res.send(Bread[req.params.arrayIndex]);
+  if (Bread[req.params.arrayIndex]) {
+    res.render("Show", {
+      bread: Bread[req.params.arrayIndex],
+    });
+  } else {
+    res.send("404");
+  }
 });
 
 module.exports = breads;
